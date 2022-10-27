ca85c8ed8e2227f21dbe61339fce4a2694261d5b
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2022-10-27 15:26:35 -0500

    Added producer demo

---
+++ kafka-basics/src/main/java/org/daniel107x/kafka/ProducerDemo.java
@@ -0,0 +1,38 @@
+package org.daniel107x.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger logger = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        logger.info("I'm a kafka producer");
+
+        // Create producer properties
+        Properties properties =new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create a producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello world");
+
+        // Send data - async operation (Non blocking)
+        producer.send(producerRecord);
+
+        // Flush and close producer (Flush y a synchronous operation)(until all data is actually sent)
+        producer.flush();
+        producer.close();
+    }
+}
