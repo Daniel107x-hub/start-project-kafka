140ed0dd9ace488fbaaabb0bbf40dae332cc9684
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2022-12-06 07:48:42 -0600

    Added batching to wikimedia producer

--- kafka-producer-wikimedia/src/main/java/org/daniel107/kafka/wikimedia/WikiMediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/org/daniel107/kafka/wikimedia/WikiMediaChangesProducer.java
@@ -19,6 +19,17 @@ public class WikiMediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // Set properties manually
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
+        // Set some high throughput configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
+
+
         // Create producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
